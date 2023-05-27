3b4f7476653f141bdbe61c063cd7480f1ad0b89e
Thomas Wilbur <twilbur@umich.edu>
2023-05-27 14:02:22 -0400

    adding python initially.

---
+++ alphabetize_sports_teams.py
@@ -0,0 +1,12 @@
+
+
+
+nfl_ teams = [
+"Arizona Cardinals","Atlanta Falcons","CarolinaPanthers","ChicagoBears","DallasCowboys","DetroitLions","Green Bay Packers",
+"Los Angeles Rams","Minnesota Vikings","New Orleans Saints","New York Giants","Philadelphia Eagles","San Francisco 49ers",
+"Seattle Seahawks","Tampa Bay Buccaneers","Washington Commanders","Baltimore Ravens","Buffalo Bills","Cincinnati Bengals",
+"Cleveland Browns","Denver Broncos","Houston Texans","Indianapolis Colts","Jacksonville Jaguars","Kansas City Chiefs",
+"Las Vegas Raiders","Los Angeles Chargers","Miami Dolphins","New England Patriots","New York Jets","Pittsburgh Steelers",
+"Tennessee Titans"]
+
+
